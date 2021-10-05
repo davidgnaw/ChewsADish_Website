a72adc08f2efed5bf52aa5c5cde2cf5e457225a9
YongqiZhang-1 <69965223+YongqiZhang-1@users.noreply.github.com>
2021-10-05 12:10:38 +0800

    Add files via upload

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import Poster from '../Poster';
+import Card from '../Card';
+
+function Home() {
+  return (
+    <>
+      <Poster />
+      <Card />
+    </>
+  );
+}
+
+export default Home;
