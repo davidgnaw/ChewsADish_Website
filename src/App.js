e39e8e4648a1ac2ebde929fbe61dd63008791c7e
Yu Fung David Wang <78752909+davidgnaw@users.noreply.github.com>
2021-10-08 18:08:12 -0400

    Add files via upload
    Updated with path to UserPage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Popular from './components/pages/Popular'
 import Gallary from './components/pages/Gallary'
+import UserPage from './components/pages/UserPage'
 import SignUp from './components/pages/SignUp'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/Popular' exact component={Popular} />
           <Route path='/Gallary' exact component={Gallary} />
+          <Route path='/UserPage' exact component={UserPage} />
           <Route path='/SignUp' exact component={SignUp} />
         </Switch>
       </Router>
