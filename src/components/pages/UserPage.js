d89482560b70bd0491350ed31981cf541011869f
Yu Fung David Wang <78752909+davidgnaw@users.noreply.github.com>
2021-10-08 18:05:01 -0400

    Add files via upload
    Userpage Component

---
+++ src/components/pages/UserPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+
+import Hero from "../userpage/Hero/Hero";
+import Projects from "../userpage/Projects/Projects";
+
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+    </>
+  );
+}
+
+export default Home;
