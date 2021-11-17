d79b9eab51ffb33308de5091ec6b128aa2723f7f
Yongqi Zhang <yongqi.zhang@emory.edu>
2021-11-16 22:48:37 -0500

    css

--- frontend/src/components/pages/IngredientWalmart.js
+++ frontend/src/components/pages/IngredientWalmart.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../../App.css';
 
 
 const IngredientWalmart = () => {
     const [searchTerm, setSearchTerm] = useState(null);
+    const [ingredients, setIngredients] = useState(null);
+    const [isFound, setIsFound] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:3456/api/substitution')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setIngredients(data)
+            })
+    }, []);
+
+    function checkIngredient(){
+        if(ingredients.filter((ingredient)=>{ingredient.ingredient == searchTerm}).length != 0){return console.log("Found")}
+        else{return window.open(`https://www.walmart.com/search?query=${searchTerm}&search_redirect=false`)}
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        window.open(`https://www.walmart.com/search?query=${searchTerm}&search_redirect=false`)
+        checkIngredient();
     }
+
+
 
     return(
         <div className="CreateYourOwnRecipe">
